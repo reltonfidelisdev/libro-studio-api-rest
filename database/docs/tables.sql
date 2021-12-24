6788111423b07141275736e6b54ea7f220f8420e
Relton Fidelis <contato.reltonfidelis@gmail.com>
2021-12-24 03:21:02 -0300

    adicionei o código sql das tabelas

--- database/docs/tables.sql
+++ database/docs/tables.sql
@@ -20,7 +20,6 @@ CREATE TABLE ` cursos ` (
   PRIMARY KEY (` id `)
 ) ENGINE = InnoDB AUTO_INCREMENT = 13 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci
 
-
 -- Cria a tabela matriculas
 CREATE TABLE ` matriculas ` (
   ` id ` bigint unsigned NOT NULL AUTO_INCREMENT,
